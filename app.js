836098c3433e8c6d7fabc68a0c0c491b7d12291b
samtasticc <samantha.henesy@gmail.com>
2024-08-15 21:26:00 -0400

    exercise 5 and 6 pass

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const inventors = [
     { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
     { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
     { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
-  ];
-  
-  const people = [
+];
+
+const people = [
     'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry',
     'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul',
     'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David',
@@ -25,40 +25,40 @@ const inventors = [
     'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank',
     'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony',
     'Blake, William'
-  ];
-  
-  const travelMethods = [
+];
+
+const travelMethods = [
     'car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van',
     'bike', 'walk', 'car', 'van', 'car', 'truck'
-  ];
+];
 
 
-  const devs = [
+const devs = [
     { name: 'Alex', year: 1988 },
     { name: 'Dani', year: 1986 },
     { name: 'Matt', year: 1970 },
     { name: 'Wes', year: 2015 }
-  ];
-  
-  const comments = [
+];
+
+const comments = [
     { text: 'Love this!', id: 523423 },
     { text: 'Super good', id: 823423 },
     { text: 'You are the best', id: 2039842 },
     { text: 'Ramen is my fav food ever', id: 123523 },
     { text: 'Nice Nice Nice!', id: 542328 }
-  ];
-  
-  /*
+];
+
+/*
 Exercise 1: Array.prototype.filter()
 
 Filter the array of inventors into a new array containing only the inventors 
 born in the 1500's.
 
 - You have an array of inventors, each with a birth year listed by the property 
-  'year'.
+'year'.
 - Use the Array.prototype.filter() method to create a new array.
 - The new array, 'veryOldInventors', should only include inventors born between 
-  the years 1500 and 1599.
+the years 1500 and 1599.
 */
 
 let veryOldInventors = []
@@ -67,17 +67,17 @@ let veryOldInventors = []
 
 
 veryOldInventors = inventors.filter(function (inventor) {
-    return inventor.year >= 1500 && inventor.year <1600;
+    return inventor.year >= 1500 && inventor.year < 1600;
 })
 
 // Check your work:
 console.log('Exercise 1 my result: ', veryOldInventors)
 console.log(
-  'Exercise 1 correct result: ',
-  [
-    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }
-  ]
+    'Exercise 1 correct result: ',
+    [
+        { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+        { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }
+    ]
 )
 
 /*
@@ -109,21 +109,21 @@ inventorNames = inventors.map(function (inventor) {
 // Check your work:
 console.log('Exercise 2 my result: ', inventorNames)
 console.log(
-  'Exercise 2 correct result: ',
-  [
-    { first: 'Albert', last: 'Einstein' },
-    { first: 'Isaac', last: 'Newton' },
-    { first: 'Galileo', last: 'Galilei' },
-    { first: 'Marie', last: 'Curie' },
-    { first: 'Johannes', last: 'Kepler' },
-    { first: 'Nicolaus', last: 'Copernicus' },
-    { first: 'Max', last: 'Planck' },
-    { first: 'Katherine', last: 'Blodgett' },
-    { first: 'Ada', last: 'Lovelace' },
-    { first: 'Sarah E.', last: 'Goode' },
-    { first: 'Lise', last: 'Meitner' },
-    { first: 'Hanna', last: 'Hammarström' }
-  ]
+    'Exercise 2 correct result: ',
+    [
+        { first: 'Albert', last: 'Einstein' },
+        { first: 'Isaac', last: 'Newton' },
+        { first: 'Galileo', last: 'Galilei' },
+        { first: 'Marie', last: 'Curie' },
+        { first: 'Johannes', last: 'Kepler' },
+        { first: 'Nicolaus', last: 'Copernicus' },
+        { first: 'Max', last: 'Planck' },
+        { first: 'Katherine', last: 'Blodgett' },
+        { first: 'Ada', last: 'Lovelace' },
+        { first: 'Sarah E.', last: 'Goode' },
+        { first: 'Lise', last: 'Meitner' },
+        { first: 'Hanna', last: 'Hammarström' }
+    ]
 )
 
 /*
@@ -144,21 +144,21 @@ sortedByBirthYear = inventors.sort(function (a, b) {
 // Check your work:
 console.log('Exercise 3 my result: ', sortedByBirthYear)
 console.log(
-  'Exercise 3 correct result: ',
-  [
-    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
-    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
-    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
-    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 }
-  ]
+    'Exercise 3 correct result: ',
+    [
+        { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+        { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+        { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+        { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+        { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+        { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+        { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+        { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+        { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+        { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+        { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+        { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 }
+    ]
 )
 
 /* 
@@ -178,13 +178,109 @@ let inventorNamedAda = {}
 
 // Complete the exercise in the space below:
 
-inventorNamedAda = inventors.find(function (inventor){
+inventorNamedAda = inventors.find(function (inventor) {
     return inventor.first === 'Ada'
 })
 
 // Check your work:
 console.log('Exercise 4 my result: ', inventorNamedAda)
 console.log(
-  'Exercise 4 correct result: ', 
-  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 }
+    'Exercise 4 correct result: ',
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 }
 )
+
+/*
+Exercise 5: Array.prototype.map()
+
+Use the Array.prototype.map() method to reformat each name in the 'people' 
+array. The goal is to convert names from "Last, First" format to "First Last" 
+format.
+
+Hint: Use the String.prototype.split() method to separate the first and last 
+      names. You can split the string using ', ' as the separator.
+      After splitting the names, rearrange them to the "First Last" format.
+*/
+
+let firstLast = []
+
+// Complete the exercise in the space below:
+
+firstLast = people.map(function (person) {
+    let splitName = person.split(', ');
+    let reformattedName = splitName[1] + ' ' + splitName[0];
+    return reformattedName;
+})
+
+// Check your work:
+console.log('Exercise 5 my result: ', firstLast)
+console.log(
+    'Exercise 5 correct result: ',
+    [
+        'Carl Becker',
+        'Samuel Beckett',
+        'Mick Beddoes',
+        'Henry Beecher',
+        'Ludwig Beethoven',
+        'Menachem Begin',
+        'Hilaire Belloc',
+        'Saul Bellow',
+        'Robert Benchley',
+        'Peter Benenson',
+        'David Ben-Gurion',
+        'Walter Benjamin',
+        'Tony Benn',
+        'Chester Bennington',
+        'Leana Benson',
+        'Silas Bent',
+        'Lloyd Bentsen',
+        'Ric Berger',
+        'Ingmar Bergman',
+        'Luciano Berio',
+        'Milton Berle',
+        'Irving Berlin',
+        'Eric Berne',
+        'Sandra Bernhard',
+        'Yogi Berra',
+        'Halle Berry',
+        'Wendell Berry',
+        'Erin Bethea',
+        'Aneurin Bevan',
+        'Ken Bevel',
+        'Joseph Biden',
+        'Ambrose Bierce',
+        'Steve Biko',
+        'Josh Billings',
+        'Frank Biondo',
+        'Augustine Birrell',
+        'Elk Black',
+        'Robert Blair',
+        'Tony Blair',
+        'William Blake'
+    ]
+)
+
+/*
+Exercise 6: Array.prototype.some()
+
+Determine if there is at least one person in the devs array who is 18 years 
+old or older.
+
+- You have an array of people with their respective ages.
+- Use the Array.prototype.some() method to check if any person in the array is 
+  18 years old or older.
+- Store the result (true or false) in the variable 'isAdultPresent'. 
+*/
+
+let isAdultPresent = null
+
+// Complete the exercise in the space below:
+
+isAdultPresent = devs.some(function (dev) {
+    const currentYear = new Date().getFullYear()
+    const age = currentYear - dev.years;
+    return age >= 18
+})
+
+// Check your work:
+console.log('Exercise 6 my result: ', isAdultPresent)
+console.log('Exercise 6 correct result: ', true)
